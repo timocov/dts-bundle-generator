326690bbb72d1d646f69cf777a7d54b3d21450c1
Eugene Timokhov <timocov@gmail.com>
2018-02-04 00:49:13 +0300

    Added tests for saving jsdoc

---
+++ tests/test-cases/save-jsdoc/input.ts
@@ -0,0 +1,31 @@
+import { NonExportedClass } from './some-class';
+
+/**
+ * ExportedInterface JSDoc
+ */
+export interface ExportedInterface {}
+
+/**
+ * ExportedType JSDoc
+ */
+export type ExportedType = string | number;
+
+/**
+ * ExportedConstEnum JSDoc
+ */
+export const enum ExportedConstEnum { Item }
+
+/**
+ * ExportedEnum JSDoc
+ */
+export enum ExportedEnum { Item }
+
+/**
+ * const JSDoc
+ */
+export const constItem = 1;
+
+/**
+ * ExportedClass JSDoc
+ */
+export class ExportedClass extends NonExportedClass {}
---
+++ tests/test-cases/save-jsdoc/output.d.ts
@@ -0,0 +1,43 @@
+declare enum NonExportedEnum {
+	First = 0,
+}
+/**
+ * NonExportedConstEnum JSDoc
+ */
+export declare const enum NonExportedConstEnum {
+	First = 0,
+}
+declare class NonExportedClass {
+	method(): NonExportedEnum;
+	method2(): NonExportedConstEnum;
+}
+/**
+ * ExportedInterface JSDoc
+ */
+export interface ExportedInterface {
+}
+/**
+ * ExportedType JSDoc
+ */
+export declare type ExportedType = string | number;
+/**
+ * ExportedConstEnum JSDoc
+ */
+export declare const enum ExportedConstEnum {
+	Item = 0,
+}
+/**
+ * ExportedEnum JSDoc
+ */
+export declare enum ExportedEnum {
+	Item = 0,
+}
+/**
+ * const JSDoc
+ */
+export declare const constItem = 1;
+/**
+ * ExportedClass JSDoc
+ */
+export declare class ExportedClass extends NonExportedClass {
+}
