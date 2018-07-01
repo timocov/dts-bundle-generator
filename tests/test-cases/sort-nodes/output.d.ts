633f7e552056068fa269c54925dea346a9b569fb
Eugene Timokhov <timocov@gmail.com>
2018-07-02 02:02:13 +0300

    Do not use jsdoc while sorting nodes
    Fixes #23

--- tests/test-cases/sort-nodes/output.d.ts
+++ tests/test-cases/sort-nodes/output.d.ts
@@ -1,5 +1,9 @@
 export declare type ATypeName = string | number;
 export declare type BType = number;
+/**
+ * Some description
+ */
+export declare type ZTypeName = string | null;
 export interface BInterfaceName {
 	prop: number;
 	prop2: ATypeName;
