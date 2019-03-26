a4e0762410aad0de0b57915503482b960f570440
Evgeniy Timokhov <timocov@gmail.com>
2019-03-26 03:19:18 +0300

    Added support to detect non-exported variables

--- src/helpers/check-non-exported-emitted-in-js-statements.ts
+++ src/helpers/check-non-exported-emitted-in-js-statements.ts
@@ -6,46 +6,65 @@ import {
 	SourceFileExport,
 } from './typescript';
 
-type EmittedInJsDeclaration = ts.ClassDeclaration | ts.EnumDeclaration | ts.FunctionDeclaration;
+type EmittedInJsDeclaration = ts.ClassDeclaration | ts.EnumDeclaration | ts.FunctionDeclaration | ts.VariableDeclaration;
 
 export function checkNonExportedEmittedInJsStatements(
 	statements: ts.Statement[],
 	rootFileExports: ReadonlyArray<SourceFileExport>,
 	typeChecker: ts.TypeChecker
 ): void {
-	const nonExportedExportableStatements = statements
-		.filter((statement: ts.Statement) => {
-			// interfaces, types, module declarations, etc doesn't affect JS runtime
-			// so we don't want to check them - only classes, enums (not const) and functions (maybe something else?)
-			if (!ts.isClassDeclaration(statement) && !ts.isEnumDeclaration(statement) && !ts.isFunctionDeclaration(statement)) {
-				return false;
-			}
+	const nonExportedExportableDeclarations: EmittedInJsDeclaration[] = [];
 
-			const exportType = getExportTypeForDeclaration(rootFileExports, typeChecker, statement);
+	for (const statement of statements) {
+		// interfaces, types, module declarations, etc doesn't affect JS runtime
+		// so we don't want to check them - only classes, enums (not const) and functions (maybe something else?)
+		if (!ts.isClassDeclaration(statement) &&
+			!ts.isEnumDeclaration(statement) &&
+			!ts.isFunctionDeclaration(statement) &&
+			!ts.isVariableStatement(statement)
+		) {
+			continue;
+		}
+
+		const statementDeclarations = ts.isVariableStatement(statement)
+			? statement.declarationList.declarations
+			: [statement];
+
+		for (const declaration of statementDeclarations) {
+			const exportType = getExportTypeForDeclaration(rootFileExports, typeChecker, declaration);
 			const isConstEnum = ts.isEnumDeclaration(statement) && hasNodeModifier(statement, ts.SyntaxKind.ConstKeyword);
 
-			// const enum always can be exported
-			return exportType === null && !isConstEnum;
-		}) as EmittedInJsDeclaration[];
+			if (exportType === null && !isConstEnum) {
+				nonExportedExportableDeclarations.push(declaration);
+			}
+		}
+	}
 
-	if (nonExportedExportableStatements.length === 0) {
+	if (nonExportedExportableDeclarations.length === 0) {
 		return;
 	}
 
-	const errorSummary = `Generated dts contains ${nonExportedExportableStatements.length} non-exported declaration(s), which should be either exported or removed to avoid runtime errors`;
+	const errorSummary = `Generated dts contains ${nonExportedExportableDeclarations.length} non-exported declaration(s), which should be either exported or removed to avoid runtime errors`;
 
-	const statementsText = nonExportedExportableStatements
-		.map((statement: EmittedInJsDeclaration) => {
-			const name = statement.name === undefined
-				? `anonymous ${typeToString(statement.kind)}`
-				: `${statement.name.text} ${typeToString(statement.kind)}`;
-			return `${name} from ${statement.getSourceFile().fileName}`;
-		})
+	const statementsText = nonExportedExportableDeclarations
+		.map((statement: EmittedInJsDeclaration) => `${getDeclarationName(statement)} from ${statement.getSourceFile().fileName}`)
 		.join('\n');
 
 	throw new Error(`${errorSummary}:
 ${statementsText}
 See https://github.com/timocov/dts-bundle-generator/issues/58 for more information.`);
+}
+
+function getDeclarationName(declaration: EmittedInJsDeclaration): string {
+	if (declaration.name === undefined) {
+		return `anonymous ${typeToString(declaration.kind)}`;
+	}
+
+	if (!ts.isIdentifier(declaration.name)) {
+		throw new Error(`Cannot get name for declaration ${declaration.getText()}`);
+	}
+
+	return `"${declaration.name.text}" ${typeToString(declaration.kind)}`;
 }
 
 function typeToString(type: EmittedInJsDeclaration['kind']): string {
@@ -58,5 +77,8 @@ function typeToString(type: EmittedInJsDeclaration['kind']): string {
 
 		case ts.SyntaxKind.FunctionDeclaration:
 			return 'function';
+
+		case ts.SyntaxKind.VariableDeclaration:
+			return 'variable';
 	}
 }
