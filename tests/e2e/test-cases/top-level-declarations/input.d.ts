2e19406211c182be2db6607d60e236126667d917
Evgeniy Timokhov <timocov@gmail.com>
2020-04-22 01:07:10 +0300

    Added semicolon to top-level-declarations test case

--- tests/e2e/test-cases/top-level-declarations/input.d.ts
+++ tests/e2e/test-cases/top-level-declarations/input.d.ts
@@ -1,9 +1,9 @@
 import { variable, SampleClass, ISample, SampleNS, functionName } from './library';
 
 export namespace Sample {
-	export { functionName }
-	export { variable }
-	export { SampleClass }
-	export { ISample }
-	export { SampleNS }
+	export { functionName };
+	export { variable };
+	export { SampleClass };
+	export { ISample };
+	export { SampleNS };
 }
--- tests/e2e/test-cases/top-level-declarations/output.d.ts
+++ tests/e2e/test-cases/top-level-declarations/output.d.ts
@@ -4,11 +4,11 @@ declare class SampleClass {}
 export interface ISample {}
 export namespace SampleNS {}
 export namespace Sample {
-	export { functionName }
-	export { variable }
-	export { SampleClass }
-	export { ISample }
-	export { SampleNS }
+	export { functionName };
+	export { variable };
+	export { SampleClass };
+	export { ISample };
+	export { SampleNS };
 }
 
 export {};
