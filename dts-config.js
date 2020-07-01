9605695678165a716a165aef6e9711c8db540f14
Evgeniy Timokhov <timocov@gmail.com>
2020-07-02 00:32:15 +0300

    Introduce composite projects
    1. Changed structure of published package (breaking change)
    2. Dropped clean-publish package

--- dts-config.js
+++ dts-config.js
@@ -3,15 +3,15 @@
 /** @type import('./src/config-file/load-config-file').BundlerConfig */
 const config = {
 	compilationOptions: {
-		preferredConfigPath: './tsconfig.json',
+		preferredConfigPath: './tsconfig.options.json',
 	},
 	entries: [
 		{
-			filePath: './lib/src/bundle-generator.d.ts',
+			filePath: './dts-out/bundle-generator.d.ts',
 			outFile: './dist/bundle-generator.d.ts',
 		},
 		{
-			filePath: './lib/src/config-file/load-config-file.d.ts',
+			filePath: './dts-out/config-file/load-config-file.d.ts',
 			outFile: './dist/config-schema.d.ts',
 		},
 	],
