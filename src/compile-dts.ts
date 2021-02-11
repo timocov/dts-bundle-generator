5c7771e845d66e036e34ad941c09003e92ba8036
Evgeniy Timokhov <timocov@gmail.com>
2021-02-11 21:51:17 +0300

    Removed `declarationDir` and `composite` from compiler options
    `declarationDir` can be easily dropped without any issue, but `composite` isn't supported right now, see #93
    
    Fixes #147

--- src/compile-dts.ts
+++ src/compile-dts.ts
@@ -21,6 +21,12 @@ export function compileDts(rootFiles: ReadonlyArray<string>, preferredConfigPath
 	compilerOptions.outDir = undefined;
 	compilerOptions.incremental = undefined;
 	compilerOptions.tsBuildInfoFile = undefined;
+	compilerOptions.declarationDir = undefined;
+
+	if (compilerOptions.composite) {
+		warnLog(`Composite projects aren't supported at the time. Prefer to use non-composite project to generate declarations instead or just ignore this message if everything works fine. See https://github.com/timocov/dts-bundle-generator/issues/93`);
+		compilerOptions.composite = undefined;
+	}
 
 	const dtsFiles = getDeclarationFiles(rootFiles, compilerOptions);
 
