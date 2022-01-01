timocov/dts-bundle-generator

commit 726630787cae2ff78f879653d534c4a2935854fb

Added handling re-export with renamings
Fixes #117

src/helpers/typescript.ts
@@ -130,6 +130,7 @@ export const enum ExportType {
 }
 
 export interface SourceFileExport {
+	exportedName: string;
 	symbol: ts.Symbol;
 	type: ExportType;
 }
@@ -142,6 +143,7 @@ export function getExportsForSourceFile(typeChecker: ts.TypeChecker, sourceFileS
 				{
 					symbol: getActualSymbol(commonJsExport, typeChecker),
 					type: ExportType.CommonJS,
+					exportedName: '',
 				},
 			];
 		}
@@ -149,7 +151,7 @@ export function getExportsForSourceFile(typeChecker: ts.TypeChecker, sourceFileS
 
 	const result: SourceFileExport[] = typeChecker
 		.getExportsOfModule(sourceFileSymbol)
-		.map((symbol: ts.Symbol) => ({ symbol, type: ExportType.ES6Named }));
+		.map((symbol: ts.Symbol) => ({ symbol, exportedName: symbol.escapedName as string, type: ExportType.ES6Named }));
 
 	if (sourceFileSymbol.exports !== undefined) {
 		const defaultExportSymbol = sourceFileSymbol.exports.get(ts.InternalSymbolName.Default);
@@ -163,6 +165,7 @@ export function getExportsForSourceFile(typeChecker: ts.TypeChecker, sourceFileS
 				result.push({
 					symbol: defaultExportSymbol,
 					type: ExportType.ES6Default,
+					exportedName: 'default',
 				});
 			}
 		}
@@ -175,59 +178,54 @@ export function getExportsForSourceFile(typeChecker: ts.TypeChecker, sourceFileS
 	return result;
 }
 
-export function getExportTypeForDeclaration(
+export function getExportsForStatement(
 	exportedSymbols: ReadonlyArray<SourceFileExport>,
 	typeChecker: ts.TypeChecker,
-	declaration: ts.NamedDeclaration | ts.VariableStatement
-): ExportType | null {
-	if (ts.isVariableStatement(declaration)) {
-		if (declaration.declarationList.declarations.length === 0) {
-			return null;
+	statement: ts.Statement
+): SourceFileExport[] {
+	if (ts.isVariableStatement(statement)) {
+		if (statement.declarationList.declarations.length === 0) {
+			return [];
 		}
 
-		const firstDeclarationExportType = getExportTypeForName(
+		const firstDeclarationExports = getExportsForName(
 			exportedSymbols,
 			typeChecker,
-			declaration.declarationList.declarations[0].name
+			statement.declarationList.declarations[0].name
 		);
 
-		const allDeclarationsHaveSameExportType = declaration.declarationList.declarations.every((variableDecl: ts.VariableDeclaration) => {
+		const allDeclarationsHaveSameExportType = statement.declarationList.declarations.every((variableDecl: ts.VariableDeclaration) => {
 			// all declaration should have the same export type
 			// TODO: for now it's not supported to have different type of exports
-			return getExportTypeForName(exportedSymbols, typeChecker, variableDecl.name) === firstDeclarationExportType;
+			return getExportsForName(exportedSymbols, typeChecker, variableDecl.name)[0] === firstDeclarationExports[0];
 		});
 
 		if (!allDeclarationsHaveSameExportType) {
 			// log warn?
-			return null;
+			return [];
 		}
 
-		return firstDeclarationExportType;
+		return firstDeclarationExports;
 	}
 
-	return getExportTypeForName(exportedSymbols, typeChecker, declaration.name);
+	return getExportsForName(exportedSymbols, typeChecker, (statement as unknown as ts.NamedDeclaration).name);
 }
 
-function getExportTypeForName(
+function getExportsForName(
 	exportedSymbols: ReadonlyArray<SourceFileExport>,
 	typeChecker: ts.TypeChecker,
 	name: ts.NamedDeclaration['name']
-): ExportType | null {
+): SourceFileExport[] {
 	if (name === undefined) {
-		return null;
+		return [];
 	}
 
 	if (ts.isArrayBindingPattern(name) || ts.isObjectBindingPattern(name)) {
 		// TODO: binding patterns in variable declarations are not supported for now
 		// see https://github.com/microsoft/TypeScript/issues/30598 also
-		return null;
+		return [];
 	}
 
 	const declarationSymbol = typeChecker.getSymbolAtLocation(name);
-	const exportedSymbol = exportedSymbols.find((rootExport: SourceFileExport) => rootExport.symbol === declarationSymbol);
-	if (exportedSymbol === undefined) {
-		return null;
-	}
-
-	return exportedSymbol.type;
+	return exportedSymbols.filter((rootExport: SourceFileExport) => rootExport.symbol === declarationSymbol);
 }

tests/e2e/test-cases/default-export-of-default-export/input.ts
@@ -1,2 +1,1 @@
-import MyClass from './class';
-export default MyClass;
+export { default } from './class';

tests/e2e/test-cases/default-export-of-default-export/output.d.ts
@@ -1,5 +1,4 @@
-declare class MyClass {
+export default class MyClass {
 }
-export default MyClass;
 
 export {};

tests/e2e/test-cases/re-export-from-importable-module/input.ts
@@ -1,3 +1,6 @@
 export { Server } from 'http';
 export * from 'fs';
 export { SomeInterface } from './interface';
+export { SomeInterface as Int1, AnotherInterface as Int2, default as DefInterface } from './interface';
+export { constName as cName, letName as lName, funcName as fName, default as defFunction } from './variables';
+export { default as MyClass } from './class';

tests/e2e/test-cases/re-export-from-importable-module/output.d.ts
@@ -1,7 +1,30 @@
 export interface SomeInterface {
 	field: string;
 }
+interface AnotherInterface {
+	field: number;
+}
+interface DefaultInterface {
+	field: boolean;
+}
+declare let letName: number;
+declare const constName = "const";
+declare function funcName(): void;
+declare function defaultFunction(): void;
+declare class MyClass {
+}
 export { Server } from 'http';
 export * from 'fs';
+
+export {
+	AnotherInterface as Int2,
+	DefaultInterface as DefInterface,
+	MyClass as MyClass,
+	SomeInterface as Int1,
+	constName as cName,
+	defaultFunction as defFunction,
+	funcName as fName,
+	letName as lName,
+};
 
 export {};

src/bundle-generator.ts
@@ -4,11 +4,10 @@ import * as path from 'path';
 import { compileDts } from './compile-dts';
 import { TypesUsageEvaluator } from './types-usage-evaluator';
 import {
-	ExportType,
 	getActualSymbol,
 	getDeclarationsForSymbol,
 	getExportsForSourceFile,
-	getExportTypeForDeclaration,
+	getExportsForStatement,
 	hasNodeModifier,
 	isAmbientModule,
 	isDeclarationFromExternalModule,
@@ -140,6 +139,7 @@ export function generateDtsBundle(entries: ReadonlyArray<EntryPointConfig>, opti
 
 	const typesUsageEvaluator = new TypesUsageEvaluator(sourceFiles, typeChecker);
 
+	// tslint:disable-next-line:cyclomatic-complexity
 	return entries.map((entry: EntryPointConfig) => {
 		normalLog(`Processing ${entry.filePath}`);
 
@@ -170,6 +170,7 @@ export function generateDtsBundle(entries: ReadonlyArray<EntryPointConfig>, opti
 			typesReferences: new Set(),
 			imports: new Map(),
 			statements: [],
+			renamedExports: [],
 		};
 
 		const outputOptions: OutputOptions = entry.output || {};
@@ -188,6 +189,24 @@ export function generateDtsBundle(entries: ReadonlyArray<EntryPointConfig>, opti
 			shouldDeclareGlobalBeInlined: (currentModule: ModuleInfo) => Boolean(outputOptions.inlineDeclareGlobals) && currentModule.type === ModuleType.ShouldBeInlined,
 			shouldDeclareExternalModuleBeInlined: () => Boolean(outputOptions.inlineDeclareExternals),
 			getModuleInfo: (fileName: string) => getModuleInfo(fileName, criteria),
+			resolveIdentifier: (identifier: ts.Identifier) => {
+				const symbol = getDeclarationNameSymbol(identifier, typeChecker);
+				if (symbol === null) {
+					return undefined;
+				}
+
+				const declarations = getDeclarationsForSymbol(symbol);
+				if (declarations.length === 0) {
+					return undefined;
+				}
+
+				const decl = declarations[0];
+				if (!isNodeNamedDeclaration(decl)) {
+					return undefined;
+				}
+
+				return decl.name;
+			},
 			getDeclarationsForExportedAssignment: (exportAssignment: ts.ExportAssignment) => {
 				const symbolForExpression = typeChecker.getSymbolAtLocation(exportAssignment.expression);
 				if (symbolForExpression === undefined) {
@@ -243,27 +262,35 @@ export function generateDtsBundle(entries: ReadonlyArray<EntryPointConfig>, opti
 		return generateOutput(
 			{
 				...collectionResult,
-				needStripDefaultKeywordForStatement: (statement: ts.Statement) => statement.getSourceFile() !== rootSourceFile,
+				needStripDefaultKeywordForStatement: (statement: ts.Statement) => {
+					const statementExports = getExportsForStatement(rootFileExports, typeChecker, statement);
+
+					// if true - no direct export was found
+					// that means that node might have an export keyword (like interface, type, etc)
+					// otherwise, if there are only re-exports with renaming (like export { foo as bar })
+					// we don't need to put export keyword for this statement
+					// because we'll re-export it in the way
+					return statementExports.find((exp: SourceFileExport) => exp.exportedName === 'default') === undefined;
+				},
+
 				shouldStatementHasExportKeyword: (statement: ts.Statement) => {
-					let result = true;
+					const statementExports = getExportsForStatement(rootFileExports, typeChecker, statement);
+
+					// if true - no direct export was found
+					// that means that node might have an export keyword (like interface, type, etc)
+					// otherwise, if there are only re-exports with renaming (like export { foo as bar })
+					// we don't need to put export keyword for this statement
+					// because we'll re-export it in the way
+					let result = statementExports.length === 0 || statementExports.find((exp: SourceFileExport) => exp.exportedName === exp.symbol.escapedName) !== undefined;
 
 					if (ts.isClassDeclaration(statement)
 						|| ts.isEnumDeclaration(statement)
 						|| ts.isFunctionDeclaration(statement)
 						|| ts.isVariableStatement(statement)
 					) {
-						const isStatementFromRootFile = statement.getSourceFile() === rootSourceFile;
-						const exportType = getExportTypeForDeclaration(rootFileExports, typeChecker, statement);
-
-						const isExportedAsES6NamedExport = exportType === ExportType.ES6Named;
-						// the node should have `export` keyword if it is exported directly from root file (not transitive from other module)
-						const isExportedAsES6DefaultExport =
-							exportType === ExportType.ES6Default
-							&& isStatementFromRootFile
-							&& hasNodeModifier(statement, ts.SyntaxKind.ExportKeyword);
-
-						// not every class, enum and function can be exported (only exported as es6 export from root file)
-						result = isExportedAsES6NamedExport || isExportedAsES6DefaultExport;
+						// "valuable" statements must be re-exported from root source file
+						// to having export keyword in declaration fle
+						result = result && statementExports.length !== 0;
 
 						if (ts.isEnumDeclaration(statement)) {
 							// const enum always can be exported
@@ -301,6 +328,7 @@ interface CollectingResult {
 	typesReferences: Set<string>;
 	imports: Map<string, ModuleImportsSet>;
 	statements: ts.Statement[];
+	renamedExports: string[];
 }
 
 interface UpdateParams {
@@ -311,6 +339,11 @@ interface UpdateParams {
 	shouldDeclareGlobalBeInlined(currentModule: ModuleInfo, statement: ts.ModuleDeclaration): boolean;
 	shouldDeclareExternalModuleBeInlined(): boolean;
 	getModuleInfo(fileName: string): ModuleInfo;
+	/**
+	 * Returns original name which is referenced by passed identifier.
+	 * Could be used to resolve "default" identifier in exports.
+	 */
+	resolveIdentifier(identifier: ts.NamedDeclaration['name']): ts.NamedDeclaration['name'];
 	getDeclarationsForExportedAssignment(exportAssignment: ts.ExportAssignment): ts.Declaration[];
 	getDeclarationUsagesSourceFiles(declaration: ts.NamedDeclaration): Set<ts.SourceFile>;
 	areDeclarationSame(a: ts.NamedDeclaration, b: ts.NamedDeclaration): boolean;
@@ -370,6 +403,7 @@ function updateResult(params: UpdateParams, result: CollectingResult): void {
 	}
 }
 
+// tslint:disable-next-line:cyclomatic-complexity
 function updateResultForRootSourceFile(params: UpdateParams, result: CollectingResult): void {
 	function isReExportFromImportableModule(statement: ts.Statement): boolean {
 		if (!ts.isExportDeclaration(statement) || statement.moduleSpecifier === undefined || !ts.isStringLiteral(statement.moduleSpecifier)) {
@@ -390,6 +424,30 @@ function updateResultForRootSourceFile(params: UpdateParams, result: CollectingR
 
 		if (isExportAssignment || isReExportFromImportable) {
 			result.statements.push(statement);
+		}
+
+		// export { foo, bar, baz as fooBar }
+		if (ts.isExportDeclaration(statement) && statement.exportClause !== undefined && ts.isNamedExports(statement.exportClause)) {
+			for (const exportItem of statement.exportClause.elements) {
+				if (exportItem.name.getText() === 'default' && exportItem.propertyName === undefined) {
+					// export { default }
+					// return export { /get name of exportItem.name's symbol node/ as default };
+					// it seems unnecessary?
+					continue;
+				}
+
+				if (exportItem.propertyName !== undefined && exportItem.propertyName.getText() === 'default') {
+					const resolvedIdentifier = params.resolveIdentifier(exportItem.propertyName);
+					// export { default as name }
+					result.renamedExports.push(`${resolvedIdentifier?.getText() || ''} as ${exportItem.name.getText()}`);
+					continue;
+				}
+
+				// export { baz as propertyName }
+				if (exportItem.propertyName !== undefined) {
+					result.renamedExports.push(exportItem.getText());
+				}
+			}
 		}
 	}
 }
@@ -646,12 +704,17 @@ function isSourceFileDefaultLibrary(program: ts.Program, file: ts.SourceFile): b
 	return (program as CompatibleProgram).isSourceFileDefaultLibrary(file);
 }
 
-function getNodeSymbol(node: ts.NamedDeclaration, typeChecker: ts.TypeChecker): ts.Symbol | null {
-	if (node.name === undefined) {
+function getNodeSymbol(node: ts.Node, typeChecker: ts.TypeChecker): ts.Symbol | null {
+	const nodeName = (node as unknown as ts.NamedDeclaration).name;
+	if (nodeName === undefined) {
 		return null;
 	}
 
-	const symbol = typeChecker.getSymbolAtLocation(node.name);
+	return getDeclarationNameSymbol(nodeName, typeChecker);
+}
+
+function getDeclarationNameSymbol(name: ts.DeclarationName, typeChecker: ts.TypeChecker): ts.Symbol | null {
+	const symbol = typeChecker.getSymbolAtLocation(name);
 	if (symbol === undefined) {
 		return null;
 	}

src/generate-output.ts
@@ -13,6 +13,7 @@ export interface OutputParams extends OutputHelpers {
 	typesReferences: Set<string>;
 	imports: Map<string, ModuleImportsSet>;
 	statements: ReadonlyArray<ts.Statement>;
+	renamedExports: string[];
 }
 
 export interface OutputHelpers {
@@ -62,6 +63,10 @@ export function generateOutput(params: OutputParams, options: OutputOptions = {}
 	}
 
 	resultOutput += statements.map(statementTextToString).join('\n');
+
+	if (params.renamedExports.length !== 0) {
+		resultOutput += `\n\nexport {\n\t${params.renamedExports.sort().join(',\n\t')},\n};`;
+	}
 
 	if (options.umdModuleName !== undefined) {
 		resultOutput += `\n\nexport as namespace ${options.umdModuleName};`;

tests/e2e/test-cases/re-export-from-importable-module/class.ts
@@ -1,0 +1,1 @@
+export default class MyClass {}

commit 49a9b071d82053264984918c2c38b98ad2d8848f

Fixed incorrect check of export type
Broken in 726630787cae2ff78f879653d534c4a2935854fb

Fixes #181

src/helpers/typescript.ts
@@ -251,7 +251,7 @@ export function getExportsForStatement(
 		const allDeclarationsHaveSameExportType = statement.declarationList.declarations.every((variableDecl: ts.VariableDeclaration) => {
 			// all declaration should have the same export type
 			// TODO: for now it's not supported to have different type of exports
-			return getExportsForName(exportedSymbols, typeChecker, variableDecl.name)[0] === firstDeclarationExports[0];
+			return getExportsForName(exportedSymbols, typeChecker, variableDecl.name)[0]?.type === firstDeclarationExports[0]?.type;
 		});
 
 		if (!allDeclarationsHaveSameExportType) {

tests/e2e/test-cases/export-object-with-destructuring/config.ts
@@ -1,0 +1,5 @@
+import { TestCaseConfig } from '../../test-cases/test-case-config';
+
+const config: TestCaseConfig = {};
+
+export = config;

tests/e2e/test-cases/export-object-with-destructuring/input.ts
@@ -1,0 +1,9 @@
+export const queryDict: Record<string, string | boolean> = {
+	param: '123',
+	otherParam: '456',
+};
+
+export const {
+	param,
+	otherParam,
+} = queryDict;

tests/e2e/test-cases/export-object-with-destructuring/output.d.ts
@@ -1,0 +1,4 @@
+export declare const queryDict: Record<string, string | boolean>;
+export declare const param: string | boolean, otherParam: string | boolean;
+
+export {};