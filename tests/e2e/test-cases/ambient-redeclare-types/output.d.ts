timocov/dts-bundle-generator

commit 56b22bc6cd4ab54fca7f2d943121f7b8fb286d1b

Fixed incorrect handling of `declare module` statements with non-relative module name
Fixes #186

src/types-usage-evaluator.ts
@@ -1,9 +1,9 @@
 import * as ts from 'typescript';
 import {
 	getActualSymbol,
+	isDeclareModule,
 	isNamedTupleMember,
 	isNodeNamedDeclaration,
-	isRelativeDeclareModule,
 	splitTransientSymbol,
 } from './helpers/typescript';
 
@@ -59,7 +59,7 @@ export class TypesUsageEvaluator {
 	}
 
 	private computeUsageForNode(node: ts.Node): void {
-		if (isRelativeDeclareModule(node) && node.body !== undefined && ts.isModuleBlock(node.body)) {
+		if (isDeclareModule(node) && node.body !== undefined && ts.isModuleBlock(node.body)) {
 			for (const statement of node.body.statements) {
 				this.computeUsageForNode(statement);
 			}

tests/e2e/test-cases/inline-from-deps/config.ts
@@ -2,7 +2,7 @@ import { TestCaseConfig } from '../test-case-config';
 
 const config: TestCaseConfig = {
 	libraries: {
-		inlinedLibraries: ['fake-package'],
+		inlinedLibraries: ['fake-package', 'fake-types-lib-2'],
 	},
 };
 

tests/e2e/test-cases/inline-from-deps/output.d.ts
@@ -1,6 +1,12 @@
 export interface Interface {
 }
 export type Type = number | string;
+export interface Base {
+	id: string;
+}
+export interface Derived extends Base {
+	name: string;
+}
 declare class SomeClass {
 	private x;
 	public constructor();
@@ -8,5 +14,6 @@ declare class SomeClass {
 export declare type TestType = Interface | Type;
 export declare class MyClass extends SomeClass {
 }
+export declare type ReExportedTypes = Derived;
 
 export {};

src/helpers/typescript.ts
@@ -1,6 +1,5 @@
 import * as ts from 'typescript';
 
-import { fixPath } from './fix-path';
 import { getLibraryName } from './node-modules';
 
 const namedDeclarationKinds = [
@@ -108,20 +107,6 @@ export function isDeclareModule(node: ts.Node): node is ts.ModuleDeclaration {
 	// `declare module ""`, `declare global` and `namespace {}` are ModuleDeclaration
 	// but here we need to check only `declare module` statements
 	return ts.isModuleDeclaration(node) && !(node.flags & ts.NodeFlags.Namespace) && !isGlobalScopeAugmentation(node);
-}
-
-/**
- * Returns whether a node is `declare module` ModuleDeclaration with a relative path
- */
-export function isRelativeDeclareModule(node: ts.Node): node is ts.ModuleDeclaration {
-	// `declare module ""`, `declare global` and `namespace {}` are ModuleDeclaration
-	// but here we need to check only `declare module` statements
-	if (!isDeclareModule(node) || !ts.isStringLiteral(node.name)) {
-		return false;
-	}
-
-	const moduleName = fixPath(node.name.text);
-	return moduleName.startsWith('./') || moduleName.startsWith('../');
 }
 
 /**

tests/e2e/test-cases/inline-from-deps/input.ts
@@ -1,5 +1,7 @@
 import { Interface, Type } from 'fake-package';
+import { Derived } from 'fake-types-lib-2';
 import { SomeClass } from 'fake-package/some-class';
 
 export type TestType = Interface | Type;
 export class MyClass extends SomeClass {}
+export type ReExportedTypes = Derived;

commit 94ad7701668fbc5172747ca3785cb5bda9d5d922

Fixed skipping checking `declare module` statements for recursive check
As these nodes are modules and named nodes at the same time

Broken in 56b22bc6cd4ab54fca7f2d943121f7b8fb286d1b

Fixes #243

src/types-usage-evaluator.ts
@@ -62,10 +62,14 @@ export class TypesUsageEvaluator {
 			for (const statement of node.body.statements) {
 				this.computeUsageForNode(statement);
 			}
-		} else if (isNodeNamedDeclaration(node) && node.name) {
+		}
+
+		if (isNodeNamedDeclaration(node) && node.name) {
 			const childSymbol = this.getSymbol(node.name);
 			this.computeUsagesRecursively(node, childSymbol);
-		} else if (ts.isVariableStatement(node)) {
+		}
+
+		if (ts.isVariableStatement(node)) {
 			for (const varDeclaration of node.declarationList.declarations) {
 				this.computeUsageForNode(varDeclaration);
 			}

tests/e2e/test-cases/ambient-redeclare-types/config.ts
@@ -1,0 +1,5 @@
+import { TestCaseConfig } from '../test-case-config';
+
+const config: TestCaseConfig = {};
+
+export = config;

tests/e2e/test-cases/ambient-redeclare-types/input.d.ts
@@ -1,0 +1,13 @@
+// see https://github.com/dexie/Dexie.js/blob/f6a6f29183c7a05f732397524c48ce4c1236bd6e/src/public/index.d.ts#L34-L47
+
+import { ExportedType, Type1, Type2 } from './type';
+
+interface _Type1 extends Type1 {}
+interface _Type2 extends Type2 {}
+
+declare module ExportedType {
+  interface Type1 extends _Type1 {}
+  interface Type2 extends _Type2 {}
+}
+
+export { ExportedType };

tests/e2e/test-cases/ambient-redeclare-types/output.d.ts
@@ -1,0 +1,24 @@
+export interface Type1 {
+}
+export interface Type2 {
+}
+export declare interface ExportedType {
+	Type1: {
+		prototype: Type1;
+	};
+	Type2: {
+		prototype: Type2;
+	};
+}
+export interface _Type1 extends Type1 {
+}
+export interface _Type2 extends Type2 {
+}
+export declare module ExportedType {
+	interface Type1 extends _Type1 {
+	}
+	interface Type2 extends _Type2 {
+	}
+}
+
+export {};

tests/e2e/test-cases/ambient-redeclare-types/type.d.ts
@@ -1,0 +1,7 @@
+export interface Type1 {}
+export interface Type2 {}
+
+export declare interface ExportedType {
+	Type1: { prototype: Type1 };
+	Type2: { prototype: Type2 };
+}