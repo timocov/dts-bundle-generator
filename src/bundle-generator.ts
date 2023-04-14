timocov/dts-bundle-generator

commit 95acfdf43682c5a461855797ed8cfd3311729847

Changed default behavior for `module ModuleName` statements (without quotes) and fixed wrong `export` state of references namespaces/modules
Previously, for some reason name `ModuleName` was treated the same as with quotes
i.e. it was checked whether that name should be exported.

Now every used module with such name will be inlined regardless of `--inline-declare-externals` option.

Fixes #237
Fixes #241

src/bundle-generator.ts
@@ -332,7 +332,8 @@ export function generateDtsBundle(entries: readonly EntryPointConfig[], options:
 						|| ts.isClassDeclaration(statement)
 						|| (ts.isEnumDeclaration(statement) && !hasNodeModifier(statement, ts.SyntaxKind.ConstKeyword))
 						|| ts.isFunctionDeclaration(statement)
-						|| ts.isVariableStatement(statement);
+						|| ts.isVariableStatement(statement)
+						|| ts.isModuleDeclaration(statement);
 
 					if (onlyDirectlyExportedShouldBeExported) {
 						// "valuable" statements must be re-exported from root source file
@@ -555,12 +556,8 @@ function updateResultForModuleDeclaration(moduleDecl: ts.ModuleDeclaration, para
 	let moduleInfo: ModuleInfo;
 
 	if (!ts.isStringLiteral(moduleDecl.name)) {
-		// this is an old behavior of handling `declare module Name` statements
-		// where Name is a identifier, not a string literal
-		// actually in this case I'd say we need to add a statement as-is without processing
-		// but it might be a breaking change to let's not break it yet
-		const moduleFileName = resolveModuleFileName(params.currentModule.fileName, moduleDecl.name.text);
-		moduleInfo = params.getModuleInfo(moduleFileName);
+		result.statements.push(moduleDecl);
+		return;
 	} else {
 		const referencedModule = params.resolveReferencedModule(moduleDecl);
 		if (referencedModule === null) {

tests/e2e/test-cases/dont-inline-declare-extenal-modules-in-internal-files/output.d.ts
@@ -3,6 +3,10 @@ export interface Interface {
 }
 export interface InterfaceInternal extends Interface {
 }
+export declare module ModuleName {
+	interface Interface extends InterfaceInternal {
+	}
+}
 export declare var ModuleName: {
 	prototype: Interface;
 };

tests/e2e/test-cases/export-namespaces/input.ts
@@ -1,0 +1,6 @@
+import { InternalModule, InternalNamespace, ExportedModule, ExportedNamespace } from './internals';
+
+export type A = InternalModule.MType;
+export type B = InternalNamespace.NSType;
+
+export { ExportedModule, ExportedNamespace };

tests/e2e/test-cases/export-namespaces/output.d.ts
@@ -1,0 +1,16 @@
+declare namespace InternalNamespace {
+	type NSType = string;
+}
+declare module InternalModule {
+	type MType = number;
+}
+export declare namespace ExportedNamespace {
+	type ENSType = 1 | 2;
+}
+export declare module ExportedModule {
+	type EMType = 3 | 4;
+}
+export type A = InternalModule.MType;
+export type B = InternalNamespace.NSType;
+
+export {};

tests/e2e/test-cases/import()-type/output.d.ts
@@ -2,7 +2,7 @@ export interface CustomType {
 	foo: number;
 	bar: string;
 }
-export namespace Namespace {
+declare namespace Namespace {
 	export const baz: number;
 }
 export declare type GenericType<T, U> = {};

tests/e2e/test-cases/modules-in-internal-files-without-inline-declare-globals/output.d.ts
@@ -1,5 +1,3 @@
-declare module "ambient-module" {
-}
 export interface Interface {
 	field: Interface;
 }

tests/e2e/test-cases/top-level-declarations/output.d.ts
@@ -4,7 +4,7 @@ declare class SampleClass {
 }
 export interface ISample {
 }
-export namespace SampleNS { }
+declare namespace SampleNS { }
 export namespace Sample {
 	export { functionName };
 	export { variable };

commit a7255127ddbae46f2b4a37d54404fed3c644207e

Don't inline `declare module Module` from imported dependencies unless they should be inlined explicitly
Broken in 95acfdf43682c5a461855797ed8cfd3311729847

Fixes #250

src/bundle-generator.ts
@@ -475,7 +475,14 @@ function updateResult(params: UpdateParams, result: CollectingResult): void {
 
 		if (isDeclareModule(statement)) {
 			updateResultForModuleDeclaration(statement, params, result);
-			continue;
+
+			// if a statement is `declare module "module" {}` then don't process it below
+			// as it is handled already in `updateResultForModuleDeclaration`
+			// but if it is `declare module Module {}` then it can be used in types and imports
+			// so in this case it needs to be checked for "usages" below
+			if (ts.isStringLiteral(statement.name)) {
+				continue;
+			}
 		}
 
 		if (params.currentModule.type === ModuleType.ShouldBeUsedForModulesOnly) {
@@ -614,22 +621,13 @@ function updateResultForModuleDeclaration(moduleDecl: ts.ModuleDeclaration, para
 		return;
 	}
 
-	let moduleInfo: ModuleInfo;
-
-	if (!ts.isStringLiteral(moduleDecl.name)) {
-		result.statements.push(moduleDecl);
+	const referencedModuleInfo = getReferencedModuleInfo(moduleDecl, params);
+	if (referencedModuleInfo === null) {
 		return;
-	} else {
-		const referencedModuleInfo = getReferencedModuleInfo(moduleDecl, params);
-		if (referencedModuleInfo === null) {
-			return;
-		}
-
-		moduleInfo = referencedModuleInfo;
 	}
 
 	// if we have declaration of external module inside internal one
-	if (!params.currentModule.isExternal && moduleInfo.isExternal) {
+	if (!params.currentModule.isExternal && referencedModuleInfo.isExternal) {
 		// if it's allowed - we need to just add it to result without any processing
 		if (params.shouldDeclareExternalModuleBeInlined()) {
 			result.statements.push(moduleDecl);
@@ -641,7 +639,7 @@ function updateResultForModuleDeclaration(moduleDecl: ts.ModuleDeclaration, para
 	updateResult(
 		{
 			...params,
-			currentModule: moduleInfo,
+			currentModule: referencedModuleInfo,
 			statements: moduleDecl.body.statements,
 		},
 		result

tests/e2e/test-cases/declare-module-in-internal-files/input.ts
@@ -2,6 +2,8 @@
 
 /// <reference path="./ambient-module-declaration.d.ts" />
 
+import { ModuleWithoutQuotes } from 'fake-package';
+
 import { Interface, ExportedModule } from './interface';
 
 export interface InterfaceInternal extends Interface {}
@@ -9,6 +11,7 @@ export interface InterfaceInternal extends Interface {}
 declare module ModuleName {
 	export interface Interface extends InterfaceInternal {}
 	export type Bar = ExportedModule.Foo;
+	export type Foo = ModuleWithoutQuotes.A;
 }
 
 declare var ModuleName: { prototype: Interface };

tests/e2e/test-cases/declare-module-in-internal-files/output.d.ts
@@ -1,3 +1,5 @@
+import { ModuleWithoutQuotes } from 'fake-package';
+
 declare module "ambient-module" {
 }
 export interface Interface {
@@ -12,6 +14,7 @@ export declare module ModuleName {
 	interface Interface extends InterfaceInternal {
 	}
 	type Bar = ExportedModule.Foo;
+	type Foo = ModuleWithoutQuotes.A;
 }
 export declare var ModuleName: {
 	prototype: Interface;

tests/e2e/test-cases/inline-from-deps/input.ts
@@ -1,7 +1,8 @@
-import { Interface, Type } from 'fake-package';
+import { Interface, Type, ModuleWithoutQuotes } from 'fake-package';
 import { Derived } from 'fake-types-lib-2';
 import { SomeClass } from 'fake-package/some-class';
 
 export type TestType = Interface | Type;
 export class MyClass extends SomeClass {}
 export type ReExportedTypes = Derived;
+export type T = ModuleWithoutQuotes.A;

tests/e2e/test-cases/inline-from-deps/output.d.ts
@@ -1,6 +1,9 @@
 export interface Interface {
 }
 export type Type = number | string;
+declare module ModuleWithoutQuotes {
+	export type A = string;
+}
 export interface Base {
 	id: string;
 }
@@ -15,5 +18,6 @@ export type TestType = Interface | Type;
 export declare class MyClass extends SomeClass {
 }
 export type ReExportedTypes = Derived;
+export type T = ModuleWithoutQuotes.A;
 
 export {};