3be37c918029262e0a9448afd2e485c5907b11b3
Atrue <wtf.ozu@gmail.com>
2023-11-01 12:02:20 +0300

    example of default export collision

--- tests/e2e/test-cases/export-default-unnamed-statement/output.d.ts
+++ tests/e2e/test-cases/export-default-unnamed-statement/output.d.ts
@@ -6,6 +6,11 @@ declare class __DTS_BUNDLE_GENERATOR__GENERATED_NAME$3 {
 declare class __DTS_BUNDLE_GENERATOR__GENERATED_NAME$4 {
 	second: number;
 }
+declare const _default: 0;
+declare const _default: "";
+declare const _default: {
+	type: string;
+};
 
 export {
 	__DTS_BUNDLE_GENERATOR__GENERATED_NAME$1 as myFunc1,
@@ -16,6 +21,9 @@ export {
 	__DTS_BUNDLE_GENERATOR__GENERATED_NAME$3 as myClass2,
 	__DTS_BUNDLE_GENERATOR__GENERATED_NAME$4 as myClass3,
 	__DTS_BUNDLE_GENERATOR__GENERATED_NAME$4 as myClass4,
+	_default as number,
+	_default as object,
+	_default as string,
 };
 
 export {};
